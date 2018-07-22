c8e02cda79ba257f0b595881158305a9611f6e73
hoge-nobile <sundanping@hoge.cn>
2018-07-23 00:06:30 +0800

    Update actions.js
    传值-接受值

--- vuexDemo/src/store/actions.js
+++ vuexDemo/src/store/actions.js
@@ -1,9 +1,9 @@
 import * as types from './mutation-type'
 import axios from 'axios'
 
-export const check = async ({ commit }) => {
-    //  {name,password}--------------|
-    console.log(axios)
+export const check = async ({ commit },{num}) => {
+    //  {name,password}-------------   -|
+    console.log(axios,num) // 传值 接受值
      const { data} = await axios.get('http://www.hletong.com/web/public/hletong/contents/alllist')
      const res = data.length
      console.log(data, res)
